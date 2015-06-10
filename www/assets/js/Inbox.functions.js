96ad6f1432613c42afd3be7da6485b664a244ee7
Renan Vinicius <contato@renanv.com.br>
2015-06-09 22:14:41 -0300

    Change

--- www/assets/js/Inbox.functions.js
+++ www/assets/js/Inbox.functions.js
@@ -3,42 +3,50 @@ $(document).ready(function(){
 
 	var userID = window.localStorage.getItem("userID");
 
-	// swal({   
-	// 	title: "Processando solicitação",
-	// 	text: "Aguarde...",
-	// 	timer: 1000,
-	// 	showConfirmButton: false 
-	// });
+	$(".inbox-list").hide(0);
 
-	$.ajax({ 
-		dataType : 'jsonp',
-		url      : "http://gosky.com.br/webservice/getInbox.php?userID="+userID,
-		data     : $("form").serialize(),
-		success: function(result)
-		{
-			$(".inbox-list").html("");
-			$.each(result.DADOS, function(i, val) {
-				
-				var ID      = val.ID;
-				var assunto = val.assunto;
-				var login   = val.remetente_login;
+	swal({   
+		title: "Processando solicitação",
+		text: "Aguarde...",
+		timer: 1000,
+		showConfirmButton: false 
+	},
+	function continueRequest(){
+		
+		$.ajax({ 
+			dataType : 'jsonp',
+			url      : "http://gosky.com.br/webservice/getInbox.php?userID="+userID,
+			data     : $("form").serialize(),
+			success: function(result)
+			{
+				$(".inbox-list").html("");
+				$.each(result.DADOS, function(i, val) {
+						
+					var ID      = val.ID;
+					var assunto = val.assunto;
+					var login   = val.remetente_login;
 
-				var createList = $('<div data-msg-id="'+ID+'" class="col-md-12 col-inbox"><img class="img-responsive" src="http://gosky.com.br/thumb.php?tipo=nor&w=40&h=30&img=uploads/'+login+'/profile.jpg" alt="'+login+'" /><span>'+login+'#goSky<b>'+assunto+'</b></span></div>');
-				$(".inbox-list").append(createList);
+					var createList = $('<div data-msg-id="'+ID+'" class="col-md-12 col-inbox"><img class="img-responsive" src="http://gosky.com.br/thumb.php?tipo=nor&w=40&h=30&img=uploads/'+login+'/profile.jpg" alt="'+login+'" /><span>'+login+'#goSky<b>'+assunto+'</b></span></div>');
+					$(".inbox-list").append(createList);
 
-				// GO READ PAGE
-				createList.click(function(){
-				    var msgID = $(this).attr("data-msg-id");
-				    window.localStorage.setItem("read_inbox_id", msgID);
-					    location.href="read.html";
+					$(".inbox-list").fadeIn("slow");
+					swal.close();
+
+					// GO READ PAGE
+					createList.click(function(){
+					    var msgID = $(this).attr("data-msg-id");
+					    window.localStorage.setItem("read_inbox_id", msgID);
+						    location.href="read.html";
+						});
 					});
-				});
 
-		},
-		error: function()
-		{
-			alert("erro");
-		}
+			},
+			error: function()
+			{
+				alert("O servidor está ocupado, tente mais tarde.");
+			}
+		});
+
 	});
 
 	
--- www/assets/js/Read.functions.js
+++ www/assets/js/Read.functions.js
@@ -2,17 +2,37 @@ $(document).ready(function(){
 
 	var msgID = window.localStorage.getItem("read_inbox_id");
 
-	$(".body-message").html("Carregando...");
+	$(".read-message").hide(0);
 
-	$.ajax({ 
-		dataType : 'jsonp',
-		url      : "http://gosky.com.br/webservice/readMessage.php?msgID="+msgID,
-		data     : $("form").serialize(),
-		success: function(result)
-		{
-			$(".lead").html(result.ASSUNTO);
-			$(".text-message").html(result.MSG);
-		}
+	swal({   
+		title: "Processando solicitação",
+		text: "Aguarde...",
+		timer: 1000,
+		showConfirmButton: false 
+	},
+	function continueRequest(){
+		
+		$.ajax({ 
+			dataType : 'jsonp',
+			url      : "http://gosky.com.br/webservice/readMessage.php?msgID="+msgID,
+			data     : $("form").serialize(),
+			success: function(result)
+			{
+				$(".lead").html(result.ASSUNTO);
+				$(".remetente").html("De: <strong>"+result.REMETENTE+"</strong>");
+				$(".data").html("Em: <strong>"+result.DATA+"</strong>");
+				$(".lead").html(result.ASSUNTO);
+				$(".text-message").html(result.MSG);
+
+				$(".read-message").fadeIn("slow");
+				swal.close();
+			}
+		});
+	});
+
+	// REPONDER
+	$(".responder").click(function(){
+		alert("ok");
 	});
 
 });
